db36b9515f75ae32021d80023de757c9337254ba
郑伟业 <286128576@qq.com>
2025-08-04 08:45:24 +0800

    测试用例

--- file_test.go
+++ file_test.go
@@ -37,29 +37,29 @@ func Test_GetFilesByBusinessId(t *testing.T) {
 func Test_GetFileBytesById(t *testing.T) {
 	client := Create(Option{
 		Http:        "http",
-		Ip:          "10.254.47.239",
+		Ip:          "127.0.0.1",
 		Port:        9999,
 		ContextPath: "gofs",
 		AppKey:      "brain",
 		AppSecret:   "brain",
 		Env:         "prod",
 	})
-	data := client.GetFileService().GetFileBytesById("582050d6-3bdd-4b1f-a64f-70965855b7ad")
-	os.WriteFile("C:\\Work\\test_word\\upload\\zwy.pdf", data, 0777)
+	data := client.GetFileService().GetFileBytesById("347f1ee7d84642f7821163b8375cde7d")
+	os.WriteFile("C:\\Work\\test_word\\download\\zwy.png", data, 0777)
 }
 
 func Test_GetFileBytesByStorePath(t *testing.T) {
 	client := Create(Option{
 		Http:        "http",
-		Ip:          "10.254.47.239",
+		Ip:          "127.0.0.1",
 		Port:        9999,
 		ContextPath: "gofs",
 		AppKey:      "brain",
 		AppSecret:   "brain",
 		Env:         "prod",
 	})
-	data := client.GetFileService().GetFileBytesByStorePath("/temp/zwy_yf/加油站签名_20250218115431.docx")
-	os.WriteFile("C:\\Work\\test_word\\upload\\加油站签名_20250218115431.docx", data, 0777)
+	data := client.GetFileService().GetFileBytesByStorePath("temp/download/9189f17c74234e308be81d005cbbcc2fzwy.jpeg")
+	os.WriteFile("C:\\Work\\test_word\\download\\zwy_2.jpeg", data, 0777)
 }
 
 func Test_UploadTempFile(t *testing.T) {
